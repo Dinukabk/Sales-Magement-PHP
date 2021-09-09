e2f1dcdd346f2a707d61d0593164dd6b2f948c4e
DinukaBK <59777936+Dinukabk@users.noreply.github.com>
2021-09-10 05:06:34 +0530

    Add files via upload

---
+++ addcus.php
@@ -0,0 +1,108 @@
+
+
+
+
+<head></head>
+
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br><br>
+
+<br>
+
+
+<body>
+
+    <div class="row-fluid">
+        <div class="span12">
+
+         
+
+            <div class="container">
+
+                <div class="row-fluid">
+                    <div class="span2">
+                        <!-- left nav -->
+                        <ul class="nav nav-tabs nav-stacked">
+
+
+
+                        </ul>
+                        <!-- right nav -->
+                    </div>
+                    <div class="span10">
+
+                    <div class="hero-unit-3">
+                        <ul class="thumbnails">
+                            <li class="span7">
+                                <div class="thumbnail">
+                                    <div class="alert alert-info"><i class="icon-plus-sign-alt icon-large"></i>&nbsp;Add Your Virtual heare</div>
+
+                                    <form class="form-horizontal" method="POST">
+                                        <div class="control-group">
+                                            <label class="control-label" for="inputEmail">Name:</label>
+                                            <div class="controls">
+                                                <input type="text" name="un" id="inputEmail" placeholder="Class" required>
+                                            </div>
+                                        </div>
+                                        <div class="control-group">
+                                            <label class="control-label" for="inputPassword">Contact:</label>
+                                            <div class="controls">
+                                                <input type="password" name="p" id="inputPassword" placeholder="Subject" required>
+                                            </div>
+                                        </div>
+                                        <div class="control-group">
+                                            <label class="control-label" for="inputPassword">Mail:</label>
+                                            <div class="controls">
+                                                <input type="text" name="fn" id="inputPassword" placeholder="Time" required>
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="control-group">
+                                            <div class="controls">
+
+                                                <button type="submit" name="save" class="btn btn-info"><i class="icon-save icon-large"></i>&nbsp;Save Suplier</button>
+                                            </div>
+                                        </div>
+                                    </form>
+
+                                    <?php
+                                    if (isset($_POST['save'])) {
+
+                                        $un = $_POST['un'];
+                                        $p = $_POST['p'];
+                                        $fn = $_POST['fn'];
+                                        
+                                        
+                                        mysqli_query($conn,"insert into  cus (name,contact,mail) values ('$un','$p','$fn',)")or die(mysqli_error());
+                                        echo('<script>location.href = "schedule.php"</script>');
+                                    }
+                                    ?>
+
+                                </div>
+                            </li>
+
+                        </ul>
+                        </div>
+
+
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+</body>
+</html>
---
+++ cus.php
@@ -0,0 +1,134 @@
+<?php
+$conn = mysqli_connect('localhost','root','','regis')or die(mysqli_error());
+?>
+<body>
+    <style type="text/css">
+    
+    table, th, td {
+  border: 1px solid black;
+}
+table {
+    width: 100%;
+     border-collapse: collapse;
+}
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+</style>
+<br>
+<br>
+
+<br>
+
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+<br>
+
+
+
+            <div class="span9">
+                <a href="addcus.php" class="btn btn-success"><i class="icon-plus-sign icon-large"></i>&nbsp;Add customer</a>
+             
+                <br></br>
+                <div class="hero-unit-3">
+                    <table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="example">
+                        <div class="alert alert-info">
+                            <button type="button" class="close" data-dismiss="alert">&times;</button>
+                            <strong><i class="icon-user icon-large"></i>&nbsp;Class Table</strong>
+                        </div>
+                        <thead>
+                            <tr>
+                                <th>id</th>
+                                <th>name</th>
+                                <th>contact</th>
+                                <th>Mail</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $query = mysqli_query($conn,"select * from cus  ") or die(mysqli_error());
+                            while ($row = mysqli_fetch_array($query)) {
+                                $id = $row['id'];
+                                ?>
+                                <tr class="odd gradeX">
+                                    <!-- script -->
+                            <script type="text/javascript">
+                                $(document).ready(function(){
+                                    
+                                    $('#d<?php echo $id; ?>').tooltip('show')
+                                    $('#d<?php echo $id; ?>').tooltip('hide')
+                                });
+                            </script>
+                            <!-- end script -->
+                         
+                            <script type="text/javascript">
+                                $(document).ready(function(){
+                                    
+                                    $('#v<?php echo $id; ?>').tooltip('show')
+                                    $('#v<?php echo $id; ?>').tooltip('hide')
+                                });
+                            </script>
+                            <!-- end script -->
+
+                           
+                            <td><?php echo $row['id']; ?></td> 
+
+                            <td><?php echo $row['name']; ?></td> 
+                            <td><?php echo $row['contact']; ?></td> 
+                            <td><?php echo $row['mail']; ?></td> 
+                           
+                             <td width="50">
+                                <a rel="tooltip"  title="Delete Subject" id="d<?php echo $id; ?>" href="#id<?php echo $id; ?>" role="button"  data-toggle="modal" class="btn btn-danger"><i class="icon-trash icon-large"></i>&nbsp;</a>
+                               
+                            </td>
+                            <!-- user delete modal -->
+                            <div id="id<?php echo $id; ?>" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                <div class="modal-header">
+                                </div>
+                                <div class="modal-body">
+                                    <div class="alert alert-danger">Are you Sure you Want to <strong>Delete</strong>&nbsp; this Class?</div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-remove icon-large"></i>&nbsp;Close</button>
+                                    <a href="delete_sup.php<?php echo '?id=' . $class_id; ?>" class="btn btn-danger"><i class="icon-trash icon-large"></i>&nbsp;Delete</a>
+                                </div>
+                            </div>
+                            <!-- end delete modal -->
+
+                            </tr>
+                        <?php } ?>
+                        </tbody>
+                    </table>
+                    <!-- end slider -->
+                </div>
+            </div>
+
+        </div>
+    
+    </div>
+</div>
+</div>
+
+
+
+
+
+
+</body>
+</html>
+
+
